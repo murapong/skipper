f0fb695e38de40e68b4367c48f949334190e4469
murapong <murapong@gmail.com>
2013-08-04 16:12:11 +0900

    バグ修正

--- Assets/Scripts/Stage/particleMove.cs
+++ Assets/Scripts/Stage/particleMove.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using System.Collections;
 
-public class particleMove : MonoBehaviour {
-	
-	private GameObject	player;
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindWithTag("Player");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		player = GameObject.FindWithTag("Player");
-		gameObject.transform.position = player.transform.position;
-	}
+public class particleMove : MonoBehaviour
+{
+ 
+    private GameObject   player;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.FindWithTag("Player");
+        if (player)
+        {
+            gameObject.transform.position = player.transform.position;
+        }
+    }
 }
